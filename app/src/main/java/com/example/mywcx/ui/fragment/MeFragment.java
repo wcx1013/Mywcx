b597c74c7addaa8eaea661b938fa78387b299995
wcx1013 <2744729611@qq.com>
2020-03-13 12:36:24 +0800

    123TUOQw

--- app/src/main/java/com/example/mywcx/ui/fragment/MeFragment.java
+++ app/src/main/java/com/example/mywcx/ui/fragment/MeFragment.java
@@ -1,12 +1,13 @@
 package com.example.mywcx.ui.fragment;
 
+import com.example.mywcx.R;
 import com.example.mywcx.base.BaseFragment;
 import com.example.mywcx.interfaces.IBasePersenter;
 
 public class MeFragment extends BaseFragment {
     @Override
     protected int getLayout() {
-        return 0;
+        return R.layout.fragment_me;
     }
 
     @Override
